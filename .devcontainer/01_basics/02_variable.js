32cdea31cab1178c14de5e10e830e118473e5761
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-04-13 16:31:32 +0000

    data types

---
+++ .devcontainer/01_basics/02_variable.js
@@ -0,0 +1,21 @@
+ "use strict";
+ //alert(3+3) will work for browser but not nodejs
+ // console.log(3 + 4) console.log("sonia") now this will give an error so add semicolon and for readability make them in 2 sep lines
+ console.log(3+4)
+ console.log("Sonia") 
+ let name = "Sonia"
+ let age = 18
+ let isLoggedin = false 
+ //---------------primitive datatypes
+ // number => range 2^53
+ //bigint
+ // string => ""
+ // boolean => true/false
+ //null => standalone value   (empty) null is a object
+ // undefined => val not assigned is a type 
+ // symbol => unique
+ //object
+ console.log(typeof null) // object type
+ console.log(typeof undefined) //undefined type
+
+
