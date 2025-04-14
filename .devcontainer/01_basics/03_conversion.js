3f60097087b14943b0b7073c67b4d254b9220cbc
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-04-14 17:04:37 +0000

    data types /conversions

---
+++ .devcontainer/01_basics/03_conversion.js
@@ -0,0 +1,43 @@
+//  let score = '33'
+//  console.log (typeof score)
+//     //or
+// console.log(typeof (score))
+//        // type casting 
+// let val = Number(score)
+// console.log(typeof(val))
+// //  "33"=> 33
+// // "33abc" => NaN (not a number)
+// // true => 1; false => 0
+// let log  = 1
+// let b = Boolean(log)
+// console.log(b)
+// // for boolean
+// // "" => false
+// //"sonia"=> true 
+// //1=> true ; 2=> false
+// let someNumber  = 42
+// let stringConversion = String(someNumber)
+// console.log(stringConversion);
+// console.log(typeof(stringConversion));
+// //*******************************OPERATIONS *****************
+ let val = 3
+let negValue = -val
+console.log(negValue)
+let str1="hello"
+let str2 = " sonia"
+let str = str1+str2
+console.log(str);
+console.log("1"+2) // output = 12
+console.log(1 + "2") // output = 12
+console.log("1"+2+2) // output = 122 
+console.log(1+2+"2")// output = 32
+// if the string is at the beginning of the experiment everything will get converted into a string 
+//  if not then the operation gets executrd first then gets concatenated 
+console.log(true)
+///assignment operator
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+// to learn more about prefix and post fix read from js mdn
