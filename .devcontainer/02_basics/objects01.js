e0827f2f661bc810dadd712ad489a850d562519c
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-06-13 12:31:47 +0000

    obj01

---
+++ .devcontainer/02_basics/objects01.js
@@ -0,0 +1,21 @@
+//objects - has keys & values 
+
+/* there are two ways to create an obj
+->literal - will nor create a singleton
+->constructor - will create a singleton*/
+
+//singleton - no instances will be created
+
+// object-literals 
+
+const user = {} // empty objects 
+const user1 = {
+    name/*key*/:"Sonia"/*val*/,
+    age:20,
+    loc:"Nandyal",
+    email:"sonia@gmail.com",
+    isLogged:false,
+    LastLoggedInDays:["MON","FRI"]
+}
+
+console.log(user1)
