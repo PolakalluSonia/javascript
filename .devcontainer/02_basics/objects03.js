ae6db787c10f94680bcb142d6881c6578f62d979
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-06-16 11:11:29 +0000

    obj3-JSONapi-rc

---
+++ .devcontainer/02_basics/objects03.js
@@ -0,0 +1,28 @@
+//objects - deconstructing (wil be usefull for react)
+
+ const course = 
+ {
+    coursename : "abc",
+    price : "free"
+ }
+  /* easier way to extract the elements 
+  in a object  
+  we can also change it to smaller names if we want to */
+ const {coursename:name} = course
+ console.log(name)
+
+
+ /* 
+ API - the. values that we get from the backend 
+ is called API 
+ -> initially we used to get them in HTML 
+ -> but now we get then in JSON 
+ -> usually JSON is a object (it can also be array)
+ */
+
+
+ 
+ 
+ 
+
+ 
