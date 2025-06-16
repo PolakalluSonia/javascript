e5d76a96869aa7440f718d8a4d88a41c8febac74
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-06-16 13:01:54 +0000

    scope

---
+++ .devcontainer/03_basics/scope01.js
@@ -0,0 +1,27 @@
+//scope - {} ->during fun and conditionals statements not obj 
+ 
+let a = 0 ;
+const b = 20000;
+var c = 30000 ;
+console.log(a)
+console.log(b)
+console.log(c)
+
+if (true)
+{
+let a = 10 ;
+ const b = 20;
+ var c = 30 ;
+}
+console.log(a)
+console.log(b)
+console.log(c)
+
+/*
+let -  can be accessed only inside the scope 
+const - can be accessed only inside the scope 
+var - can be accessed both inside and outside
+*/
+
+
+//checking the scope in node and windows are differenrt
