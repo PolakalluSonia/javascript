31eb124e3ce3f1e6ec0bce4d718e15c4f6f92789
polakallusonia <165923879+PolakalluSonia@users.noreply.github.com>
2025-06-28 11:40:41 +0000

    while loop

---
+++ 05_loops/while.js
@@ -0,0 +1,28 @@
+//while loop 
+/*
+--------while syntax---------
+while(condition)
+{
+  statements 
+  increment / decrement (ending condition)
+}
+  ---------do while syntax---------
+ d0
+{
+  statements 
+  increment / decrement (ending condition)
+} while(condition)
+*/
+let idx = 2;
+while (idx <= 10)
+{
+    console.log(idx);
+    idx +=2
+}
+let score = 1
+do 
+{
+    console.log (score);
+    score ++;
+}
+while (score <= 10)
